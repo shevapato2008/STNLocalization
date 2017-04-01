379c82ed4e259ed50562db5cda92e0922376325f
shevapato2008 <shevapato2008@gmail.com>
2017-04-01 01:34:56 -0400

    Modified so that it can generate both rawRecording and lfpRecording.

--- mergeSgnlDpth.m
+++ mergeSgnlDpth.m
@@ -1,23 +1,31 @@
-function recording = mergeSgnlDpth(signal, depthData)
+function recording = mergeSgnlDpth(type, signal, depthData)
     % signal: 1-dimensional MER recording
     % depthData: 2-dimensional time-depth data
     % recording: 3-dimensional time-signal-depth data
-
-    for i = 1 : size(signal, 1)
-        signal(i, 2) = signal(i);
-        signal(i, 1) = i;
-    end;
+    
+    signalLen = length(signal);
+    depthLen = length(depthData);
+    recording = zeros(signalLen, 3);
+    for i = 1 : signalLen
+        if strcmp(type, 'rawSignal')
+            recording(i, 1) = depthData(1, 1) + i - 1;
+        elseif strcmp(type, 'lfp')
+            recording(i, 1) = depthData(1, 1) + (i - 1) * (48000 / 1000);
+        end
+        
+        recording(i, 2) = signal(i);
+    end
 
     key = 1;
-    len_signal = length(signal);
-    len_depth = length(depthData);
-    recording(:, 1) = signal(:, 1);
-    recording(:, 2) = signal(:, 2);
-    for i = 1 : len_signal
-        recording(i, 3) = depthData(key, 2);
-        if signal(i, 1) == depthData(key, 1)
-            if key < len_depth
+    for i = 1 : signalLen
+        if recording(i, 1) <= depthData(key, 1)
+            recording(i, 3) = depthData(key, 2);
+        else
+            if key < depthLen
+                recording(i, 3) = depthData(key + 1, 2);
                 key = key + 1;
+            else
+                recording(i, 3) = depthData(key, 2);
             end
         end
     end
