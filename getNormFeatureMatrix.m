f737c6d5cac8540357a193d86bd646d3949c39de
shevapato2008 <shevapato2008@gmail.com>
2017-04-13 15:27:06 -0400

    Initial commit.

---
+++ getNormFeatureMatrix.m
@@ -0,0 +1,22 @@
+function getNormFeatureMatrix(index, featureMatrixPath, normFeatureMatrixPath)
+% Function getNormFeatureMatrix helps generate normalized feature matrices.
+
+load(featureMatrixPath);
+numRow = size(featureMatrix, 1);
+numCol = size(featureMatrix, 2);
+normFeatureMatrix = zeros(numRow, numCol);
+
+for col = 1 : numCol
+    MIN = min(featureMatrix(:, col));
+    MAX = max(featureMatrix(:, col));
+    for row = 1 : numRow
+        normFeatureMatrix(row, col) ...
+            = (featureMatrix(row, col) - MIN) / (MAX - MIN);
+    end
+end
+
+disp(['Saving normalized featureMatrix' num2str(index) '...']);
+save(normFeatureMatrixPath, 'normFeatureMatrix', '-v7.3');
+
+end
+
