61f9c967cb34671c1a3bac73b0441aed7547f69d
shevapato2008 <shevapato2008@gmail.com>
2017-04-18 13:28:22 -0400

    Deleted or commented some of the useless code and add content to the titles.

--- designFilters.m
+++ designFilters.m
@@ -17,6 +17,7 @@ save('Filters\AAFilt_3000_3200.mat', 'AAFilt_3000_3200')
 
 
 %% Design low-pass filters
+%{
 LPFilt_110_119_6kHz = designfilt('lowpassfir', ...
     'PassbandFrequency', 110, ...       % was 185 before
     'StopbandFrequency', 119, ...
@@ -27,6 +28,7 @@ LPFilt_110_119_6kHz = designfilt('lowpassfir', ...
 fvtool(LPFilt_110_119_6kHz)
 saveas(gcf, 'Filters\LPFilt_110_119_6kHz.png')
 save('Filters\LPFilt_110_119_6kHz.mat', 'LPFilt_110_119_6kHz')
+
 
 LPFilt_110_119_48kz = designfilt('lowpassfir', ...
     'PassbandFrequency', 110, ...       % was 185 before
@@ -50,36 +52,46 @@ LPFilt_50_59 = designfilt('lowpassfir', ...
 fvtool(LPFilt_50_59)
 saveas(gcf, 'Filters\LPFilt_50_59.png')
 save('Filters\LPFilt_50_59.mat', 'LPFilt_50_59')
+%}
+
 
 
 %% Design high-pass filters
-HPFilt_385_400 = designfilt('highpassfir', ...
-    'PassbandFrequency', 400, ...
-    'StopbandFrequency', 385, ...
-    'PassbandRipple', 0.0001, ...
+HPFilt_300 = designfilt('highpassfir', ...
+    'StopbandFrequency', 285, ...
+    'PassbandFrequency', 300, ...
+    'PassbandRipple', 0.001, ...
     'StopbandAttenuation', 65, ...
-    'DesignMethod', 'equiripple', ...
-    'SampleRate', 6000);
-fvtool(HPFilt_385_400)
-saveas(gcf, 'Filters\HPFilt_385_400.png')
-save('Filters\HPFilt_385_400.mat', 'HPFilt_385_400')
+    'SampleRate', 48000, ...
+    'DesignMethod', 'equiripple');
+fvtool(HPFilt_300)
+saveas(gcf, 'Filters\HPFilt_300.png')
+save('Filters\HPFilt_300.mat', 'HPFilt_300')
 
-% Designed for filtering out 300-3000 Hz signal
-% According to R. Q. Quiroga's 2015 paper on spike filtering
-HPFilt_300_3000 = designfilt('highpassfir', ...
-    'PassbandFrequency', 3000, ...
-    'StopbandFrequency', 300, ...
-    'PassbandRipple', 0.0001, ...
-    'StopbandAttenuation', 65, ...
-    'DesignMethod', 'equiripple', ...
-    'SampleRate', 48000);
-fvtool(HPFilt_300_3000)
-saveas(gcf, 'Filters\HPFilt_300_3000.png')
-save('Filters\HPFilt_300_3000.mat', 'HPFilt_300_3000')
+% load('Filters\HPFilt_300.mat');
+% fvtool(HPFilt_300)
+% title('High Pass Filter (> 300 Hz)')
 
 
 
 %% Design band-pass filters
+HPFilt_300_3000 = designfilt('bandpassfir', ...
+                    'StopbandFrequency1', 285, 'PassbandFrequency1', 300, ...
+                    'PassbandFrequency2', 3000, 'StopbandFrequency2', 3015, ...
+                    'StopbandAttenuation1', 65, ...
+                    'PassbandRipple', 0.001, ...
+                    'StopbandAttenuation2', 65, ...
+                    'SampleRate', 48000, ...
+                    'DesignMethod', 'equiripple');
+fvtool(HPFilt_300_3000)
+title('High Pass Filter (300-3000 Hz)')
+saveas(gcf, 'Filters\HPFilt_300_3000.png')
+save('Filters\HPFilt_300_3000.mat', 'HPFilt_300_3000')
+
+% load('Filters\HPFilt_300_3000.mat');
+% fvtool(HPFilt_300_3000)
+% title('High Pass Filter (300-3000 Hz)')
+
 
 % high gamma filter 50Hz - 90Hz
 highGammaFilter = designfilt('bandpassfir', ...
@@ -91,13 +103,13 @@ highGammaFilter = designfilt('bandpassfir', ...
                     'SampleRate', 1000, ...
                     'DesignMethod', 'equiripple');
 fvtool(highGammaFilter)
-title('High Gamma Filter')
+title('High Gamma Filter (50 - 90 Hz)')
 saveas(gcf, 'Filters\highGammaFilter.png')
 save('Filters\highGammaFilter.mat', 'highGammaFilter')
 
-load('Filters\highGammaFilter.mat');
-fvtool(highGammaFilter)
-title('High Gamma Filter')
+% load('Filters\highGammaFilter.mat');
+% fvtool(highGammaFilter)
+% title('High Gamma Filter (50 - 90 Hz)')
 
 
 % low gamma filter 30Hz - 50Hz
@@ -110,13 +122,13 @@ lowGammaFilter = designfilt('bandpassfir', ...
                     'SampleRate', 1000, ...
                     'DesignMethod', 'equiripple');
 fvtool(lowGammaFilter)
-title('Low Gamma Filter')
+title('Low Gamma Filter (30 - 50 Hz)')
 saveas(gcf, 'Filters\lowGammaFilter.png')
 save('Filters\lowGammaFilter.mat', 'lowGammaFilter')
 
-load('Filters\lowGammaFilter.mat');
-fvtool(lowGammaFilter)
-title('Low Gamma Filter')
+% load('Filters\lowGammaFilter.mat');
+% fvtool(lowGammaFilter)
+% title('Low Gamma Filter (30 - 50 Hz)')
 
 
 % alpha filter 9Hz - 11Hz
@@ -129,13 +141,13 @@ alphaFilter = designfilt('bandpassfir', ...
                     'SampleRate', 1000, ...
                     'DesignMethod', 'equiripple');
 fvtool(alphaFilter)
-title('Alpha Filter')
+title('Alpha Filter (9 - 11 Hz)')
 saveas(gcf, 'Filters\alphaFilter.png')
 save('Filters\alphaFilter.mat', 'alphaFilter')
 
-load('Filters\alphaFilter.mat');
-fvtool(alphaFilter)
-title('Alpha Filter')
+% load('Filters\alphaFilter.mat');
+% fvtool(alphaFilter)
+% title('Alpha Filter (9 - 11 Hz)')
 
 
 % beta filter 13Hz - 30Hz
@@ -148,13 +160,13 @@ betaFilter = designfilt('bandpassfir', ...
                     'SampleRate', 1000, ...
                     'DesignMethod', 'equiripple');
 fvtool(betaFilter)
-title('Beta Filter')
+title('Beta Filter (13 - 30 Hz)')
 saveas(gcf, 'Filters\betaFilter.png')
 save('Filters\betaFilter.mat', 'betaFilter')
 
-load('Filters\betaFilter.mat');
-fvtool(betaFilter)
-title('Beta Filter')
+% load('Filters\betaFilter.mat');
+% fvtool(betaFilter)
+% title('Beta Filter (13 - 30 Hz)')
 
 
 % theta filter 4Hz - 8Hz
@@ -167,16 +179,16 @@ thetaFilter = designfilt('bandpassfir', ...
                     'SampleRate', 1000, ...
                     'DesignMethod', 'equiripple');
 fvtool(thetaFilter)
-title('Theta Filter')
+title('Theta Filter (4 - 8 Hz)')
 saveas(gcf, 'Filters\thetaFilter.png')
 save('Filters\thetaFilter.mat', 'thetaFilter')
 
-load('Filters\thetaFilter.mat');
-fvtool(thetaFilter)
-title('Theta Filter')
+% load('Filters\thetaFilter.mat');
+% fvtool(thetaFilter)
+% title('Theta Filter (4 - 8 Hz)')
 
 
-% delta filter 1Hz - 4Hz (NOT been run yet)
+% delta filter 1Hz - 4Hz
 deltaFilter = designfilt('bandpassfir', ...
                     'StopbandFrequency1', 0.5, 'PassbandFrequency1', 1, ...
                     'PassbandFrequency2', 4, 'StopbandFrequency2', 4.5, ...
@@ -186,13 +198,14 @@ deltaFilter = designfilt('bandpassfir', ...
                     'SampleRate', 1000, ...
                     'DesignMethod', 'equiripple');
 fvtool(deltaFilter)
-title('Delta Filter')
+title('Delta Filter (1 - 4 Hz)')
 saveas(gcf, 'Filters\deltaFilter.png')
 save('Filters\deltaFilter.mat', 'deltaFilter')
 
-load('Filters\deltaFilter.mat');
-fvtool(deltaFilter)
-title('Delta Filter')
+% load('Filters\deltaFilter.mat');
+% fvtool(deltaFilter)
+% title('Delta Filter (1 - 4 Hz)')
+
 
 % Infra-Slow Oscillation 0.1-1 Hz
 % use low-pass filter (1 1.4) instead of bandpass filter
@@ -204,11 +217,11 @@ infraSlowFilter = designfilt('lowpassfir', ...
                     'SampleRate', 1000, ...
                     'DesignMethod', 'equiripple');
 fvtool(infraSlowFilter)
-title('Infra Slow Filter')
+title('Infra Slow Filter (0 - 1 Hz)')
 saveas(gcf, 'Filters\infraSlowFilter.png')
 save('Filters\infraSlowFilter.mat', 'infraSlowFilter')
 
-load('Filters\infraSlowFilter.mat');
-fvtool(infraSlowFilter)
-title('Infra Slow Filter')
+% load('Filters\infraSlowFilter.mat');
+% fvtool(infraSlowFilter)
+% title('Infra Slow Filter (0 - 1 Hz)')
 
