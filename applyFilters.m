fb7d77b28b5648ed14033eae2b7ea52f63ed15d5
shevapato2008 <shevapato2008@gmail.com>
2017-04-18 14:10:56 -0400

    Moved the filter loading code out of the function.

--- applyFilters.m
+++ applyFilters.m
@@ -2,48 +2,7 @@ function applyFilters(rawSignal, lfp, ...
     hpfSignalPath, highGammaSignalPath, lowGammaSignalPath, ...
     alphaSignalPath, betaSignalPath, thetaSignalPath, ...
     deltaSignalPath, infraSlowSignalPath)
-      
-%% Load filters %%
-% (1) High-pass filter for Spikes - 48 kHz filter
-disp('    loading high-pass filter...');
-filter = load('Filters\HPFilt_300_3000.mat');
-    % fvtool(filter.HPFilt_300_3000)      % choose 300-3000 Hz: according to 
-    % title('High-pass filter 300-3000')  % R. Q. Quiroga's 2015 paper.
-    
-% (2) Band-pass filters for the given LFP - 1 kHz filters
-disp('    loading band-pass filters...');
-% (2.1) high gamma filter 50Hz - 90Hz
-load('Filters\highGammaFilter.mat');
-    % fvtool(highGammaFilter)
-    % title('High Gamma Filter')
-% (2.2) low gamma filter 30Hz - 50Hz
-load('Filters\lowGammaFilter.mat');
-    % fvtool(lowGammaFilter)
-    % title('Low Gamma Filter')
-% (2.3) alpha filter 9Hz - 11Hz
-load('Filters\alphaFilter.mat');
-    % fvtool(alphaFilter)
-    % title('Alpha Filter')
-% (2.4) beta filter 13Hz - 30Hz
-load('Filters\betaFilter.mat');
-    % fvtool(betaFilter)
-    % title('Beta Filter')
-% (2.5) theta filter 4Hz - 8Hz
-load('Filters\thetaFilter.mat');
-    % fvtool(thetaFilter)
-    % title('Theta Filter')
-% (2.6) delta filter 1Hz - 4Hz
-load('Filters\deltaFilter.mat');
-    % fvtool(deltaFilter)
-    % title('Delta Filter')
-% (2.7) Infra-Slow Oscillation 0.1-1 Hz
-load('Filters\infraSlowFilter.mat');
-    % fvtool(infraSlowFilter)
-    % title('Infra Slow Filter')
 
-
-%% Apply the filters %%
-disp('    Applying filters...');
 % (1) High-pass filter for Spikes - 48 kHz filter
 disp('    Generating and saving high-pass filtered signal...');
 hpfSignal = filtfilt(filter.HPFilt_300_3000, rawSignal);
