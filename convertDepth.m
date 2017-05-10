f484851e794186fd44c481abdc34c6837d833b8c
shevapato2008 <shevapato2008@gmail.com>
2017-05-09 22:05:42 -0400

    Change the code according to Dr. Wong's suggestion.

--- convertDepth.m
+++ convertDepth.m
@@ -2,16 +2,12 @@ function outputMat = convertDepth(depthMat)
 %CONVERTDEPTH Summary of this function goes here
 %   Detailed explanation goes here
 
-MIN = min(depthMat(:, 2));
-MAX = max(depthMat(:, 2));
-RANGE = MAX - MIN;
-numRow = size(depthMat, 1);
-outputMat = zeros(numRow, 3);
+nrow = size(depthMat, 1);
+outputMat = zeros(nrow, 3);
 outputMat(:, 1 : 2) = depthMat; 
 
-for i = 1 : numRow
-    PERCENT = (outputMat(i, 2) - MIN) / RANGE;
-    outputMat(i, 3) = 15 - 20 * PERCENT;
+for i = 1 : nrow
+    outputMat(i, 3) = 30 - outputMat(i, 2) / 1000;
 end
 
 end
